d9df23550195b49fbcbc5374e6c717061e761e0c
祐一 河田 <kiramicku@yahoo.co.jp>
2025-10-15 21:09:06 +0900

    Update: fix spell

--- backend/src/middleware/validation.middleware.js
+++ backend/src/middleware/validation.middleware.js
@@ -2,7 +2,7 @@ import { body, validationResult } from 'express-validator';
 
 export const validateRegistration = [
     // 1. ルールを配列として定義
-    body('mailAddress').isEmail().wihMessage('有効なメールアドレスを入力してください。'),
+    body('mailAddress').isEmail().withMessage('有効なメールアドレスを入力してください。'),
     body('password').isLength({ min: 8 }).withMessage('パスワードは8文字以上で設定してください'),
 
     // 2. バリデーション結果をチェックするミドルウェア
