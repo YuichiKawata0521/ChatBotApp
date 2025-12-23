aace1caa2e50e9d9a9cd64c79a78d4beef980f08
祐一 河田 <kiramicku@yahoo.co.jp>
2025-12-23 16:34:35 +0900

    Update: add 355M Config

--- llm/app/model/config.py
+++ llm/app/model/config.py
@@ -7,3 +7,13 @@ GPT_CONFIG_124M = {
     "drop_rate": 0.1,
     "qkv_bias": False
 }
+
+GPT_CONFIG_355M = {
+    "vocab_size": 50257,
+    "context_length": 1024,
+    "emb_dim": 1024,   # 768から1024へ
+    "n_heads": 16,     # 12から16へ
+    "n_layers": 24,    # 12から24へ
+    "drop_rate": 0.0,
+    "qkv_bias": True
+}
