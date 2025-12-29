b2c9ae5ece0a0deae3501125e20f6e369a7f1784
Yuichi Kawata <hetianyouyi235@gmail.com>
2025-12-29 10:23:44 +0900

    Update: add sendMessage

--- frontend/public/user/js/main.js
+++ frontend/public/user/js/main.js
@@ -2,6 +2,8 @@ import * as ui from './ui.js';
 import * as api from './api.js';
 import { state as configState } from './config.js';
 
+let currentConversationId = new URLSearchParams(window.location.search).get('id');
+
 async function getTexts() {
     configState.frontText = await api.getFrontText();
 }
@@ -18,6 +20,43 @@ function startNewChatListner() {
         newChatBtn.addEventListener('click', startNewChat);
     } else {
         console.error('Element with ID newChatBtn not found');
+    }
+}
+
+// メッセージ送信時の処理
+async function sendMessage() {
+    const messageInput = ui.getInputMessageId();
+    const sendButton = ui.getSendBtnId();
+
+    const message = messageInput.value.trim();
+    if (!message) return;
+
+    try {
+        // 連打無効化
+        messageInput.value = '';
+        sendButton.disable = true;
+
+        // 新規会話の場合
+        if (!currentConversationId) {
+            const res = await api.startNewConversation();
+            currentConversationId = res.newConversationId;
+
+            // URLを更新
+            const newUrl = `${window.location.pathname}?id=${currentConversationId}`;
+            window.history.replaceState(null, '', newUrl);
+        }
+
+        // ユーザーメッセージを描画
+        renderMessage('user', message);
+
+        // バックエンドへ送信
+        const response = await api.sendMessage(currentConversationId, message);
+
+        renderMessage('bot', response.reply);
+    } catch (error) {
+        console.error('送信エラー', error.message);
+    } finally {
+        sendButton.disabq = false;
     }
 }
 
